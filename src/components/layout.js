065beadf666915050c9510392cc7fd3ed0fbd9fb
Stackbit <cjcoldewey@gmail.com>
2021-02-15 11:07:12 +0000

    layout.js: updated Url 1

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="C">{title}</Link>
+        <Link to="/">{title}</Link>
       </h1>
     )
   } else {
